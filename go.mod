63f38ca5f9744a11c4e3aa4c948af5ace6e2fa10
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-16 11:45:07 +0300

    structs and some main handle functions added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/furkanadiiguzel/golang-crud-api
+
+go 1.21.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Movie Struct
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovieById).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	fmt.Print("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
